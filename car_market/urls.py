bbdd68bfddedf6b3bfa09d032c518dd26a4fa3c7
Nour El-Sharkawi <Nour@Nours-MacBook-Pro-703.local>
2019-10-08 18:04:04 +0300

    Nancy Farah

--- car_market/urls.py
+++ car_market/urls.py
@@ -1,14 +1,18 @@
 
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from cars import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cars/', views.car_list, name='car-list'),
     path('cars/<int:car_id>/', views.car_detail, name='car-detail'),
-
     path('cars/create', views.car_create, name='car-create'),
     path('cars/<int:car_id>/update/', views.car_update, name='car-update'),
     path('cars/<int:car_id>/delete/', views.car_delete, name='car-delete'),
 ]
+
+urlpatterns+=static(settings.MEDIA_URL, document_root=settings.STATIC_ROOT)
+urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Car
+from .forms import CarForm
 
 def car_list(request):
 	cars = Car.objects.all()
@@ -18,15 +19,33 @@ def car_detail(request, car_id):
 
 
 def car_create(request):
-	#Complete Me
-	return render(...)
+	form = CarForm()
+	if request.method == "POST":
+		form = CarForm(request.POST, request.FILES)
+		if form.is_valid():
+			form.save()
+			return redirect('car-list')
+	context = {
+		"form":form,
+	}
+	return render(request, 'create.html', context)
 
 
 def car_update(request, car_id):
-	#Complete Me
-	return render(...)
-
+	car_obj = Car.objects.get(id=car_id)
+	form = CarForm(instance=car_obj)
+	if request.method == "POST":
+		form = CarForm(request.POST, request.FILES, instance=car_obj)
+		if form.is_valid():
+			form.save()
+			return redirect('car-list')
+	context = {
+		"car_obj": car_obj,
+		"form":form,
+	}
+	return render(request, 'update.html', context)
 
 def car_delete(request, car_id):
-	#Complete Me
-	return render(...)
+	car_obj = Car.objects.get(id=car_id)
+	car_obj.delete()
+	return redirect('car-list')
